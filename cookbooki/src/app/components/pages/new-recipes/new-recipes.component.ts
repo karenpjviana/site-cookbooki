bdfb404131155c6ca833fc3240186dbdc2c6770c
Karen Viana <karenpjvianaa@gmail.com>
2024-06-07 23:13:47 -0300

    Initial Commit

---
+++ cookbooki/src/app/components/pages/new-recipes/new-recipes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-recipes',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-recipes.component.html',
+  styleUrl: './new-recipes.component.css'
+})
+export class NewRecipesComponent {
+
+}
